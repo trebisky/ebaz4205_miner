37bd7e00e07bfff2c5942adebe8bc2d0cb141827
Tom Trebisky <tom@mmto.org>
2022-06-04 13:02:21 -0700

    Rename the compression verifier

--- bit_util/verify_compression.c
+++ bit_util/verify_compression.c
@@ -5,11 +5,14 @@
  * This is a test fixture to see if my bitstream
  *  compress/decompress will regenerate the original data.
  */
-
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 typedef unsigned int u_int;
 
+/* This references a compressed bitstream compiled in another file.
+ */
 extern u_int pl_comp_data[];
 
 #define PL_SIZE	1024*1024
@@ -18,7 +21,7 @@ u_int pl_buffer[PL_SIZE];
 #define ZFLAG   0x80000000
 #define EFLAG   0xC0000000
 
-void
+int
 pl_expand ( u_int *dest, u_int *src )
 {
 	u_int *ip;
@@ -28,33 +31,46 @@ pl_expand ( u_int *dest, u_int *src )
 	ip = src;
 	dp = dest;
 
-	printf ( "First: %08x\n", *ip );
+	//printf ( "First: %08x\n", *ip );
 
 	while ( *ip != EFLAG ) {
-	    printf ( "Got: %08x\n", *ip );
+	    // printf ( "Got: %08x\n", *ip );
 	    if ( *ip & ZFLAG ) {
 		n = *ip & ~ZFLAG;
-		printf ( "%d zeros\n", n );
+		//printf ( "%d zeros\n", n );
 		while ( n-- )
 		    *dp++ = 0;
 		++ip;
 	    } else {
 		n = *ip++;
-		printf ( "%d data\n", n );
+		//printf ( "%d data\n", n );
 		while ( n -- )
 		    *dp++ = *ip++;
 	    }
 	}
 	n = dp - dest;
 	printf ( "Generated %d words (%d bytes)\n", n, n*4 );
+	return n*4;
 }
 
 int
 main ( int argc, char **argv )
 {
+	int nbytes;
+	int fd;
+
 	// printf ( "%d\n", sizeof(pl_comp_data) );
 	// printf ( "%08x\n", pl_comp_data[0] );
-	pl_expand ( pl_buffer, pl_comp_data );
+	nbytes = pl_expand ( pl_buffer, pl_comp_data );
+
+	fd = open ( "verify.bin", O_WRONLY | O_CREAT | O_TRUNC, 0644 );
+	if ( fd < 0 ) {
+	    printf ( "Disaster\n" );
+	    return 1;
+	}
+	write ( fd, pl_buffer, nbytes );
+	close ( fd );
+	return 0;
 }
 
 /* THE END */
